607ee280a1e860c30bd17f3e7f4b366caa3bf692
SaishruthiiD <saishruthi.dd@gmail.com>
2025-03-04 11:39:44 +0530

    longest common subsequences.c

--- longest common subsequences.c
+++ longest common subsequences.c
@@ -1,13 +1,13 @@
-int longestCommonSubsequence(char* text1, char* text2) {
-    int m = strlen(text1);
-    int n = strlen(text2);
+int longestCommonSubsequence(char* a, char* b) {
+    int m = strlen(a);
+    int n = strlen(b);
     int arr[m+1][n+1];
     for (int i = 0; i <= m; i++) {
         for (int j = 0; j <= n; j++) {
             if (i == 0 || j == 0) {
                 arr[i][j] = 0; 
             }
-            else if (text1[i-1] == text2[j-1])
+            else if (a[i-1] == b[j-1])
             {
                 arr[i][j] = arr[i-1][j-1] +1;
             }
